902e6fee09a90b7e667f64587e6f16681aa89a38
ArunachalamDeV <arunachalamuthumariappan@gmail.com>
2024-10-21 21:05:08 +0530

    first commit

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use(
   })
 );
 
+app.use("/", express.static(path.join(__dirname, "./build")));
+app.get("/",(req,res) => {
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
+})
 const multerParse = multer({
   dest: "uploads/",
   filename: (req, file, cb) => {
